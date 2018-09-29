de0feb332e774fc3dcf07d74a8693765b7a4e12a
Renan Roberto <renanroberto1@gmail.com>
2018-09-29 18:15:19 -0300

    add IO on main, remove logic file and add index.html example

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,33 @@
 module Main where
 
+import System.IO
+import System.Environment
 import Conversor
 
 main :: IO ()
 main = do
+  putStrLn "SASS Generator!\n" 
+  args <- getArgs
+  case args of
+    [] -> putStrLn "No arguments has been passed"
+    [file] -> do
+      putStrLn "Searchin file..."
+      html <- readFile file
+      putStrLn "generating style..."
+      let scss = convert html
+      writeFile "style.scss" scss
+      putStrLn "style.scss generated!"
+
+{--
   putStrLn "CSS Generator\n"
-  (putStrLn . convert) html
+  putStr "Type the name of the HTML file: "
+  file <- getLine
+  html <- readFile file
+  let scss = convert html
+  writeFile "style.scss" scss
+--}
 
-
-{--}
+{--
 html :: String
 html = "<body><div class=\"content\"><span class=\"test-1\">test 1 <div><p class=\"paragraph red\">Lorem ipsum</p></div></span><span class=\"test-2\">test 2</span><div class=\"divA\">A <p className=\"warning red\"</div><p>B</p><div>C</div></div></body>"
 --}
